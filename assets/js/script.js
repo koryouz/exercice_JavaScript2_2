e98ef92800c57dc727721698ac913aaa311b8fa5
DELHAY <delhayantoine@gmail.com>
2019-01-23 13:52:08 +0100

    Update du JS : ajout de la fonction regex

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 function myFunction() {
-  alert("Merci de votre participation");
+  var lname = document.getElementById('lastname').value;
+  var regex = new RegExp('(^[a-zA-Z]+$)');
+  if (regex.test(lname)) {
+    alert('Merci de votre participation');
   }
+  else {
+    alert('Nom invalide');
+}
+}
